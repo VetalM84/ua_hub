b121bdd6e1c9e8c311521a64bc293d9409bd27ac
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-04 20:51:57 +0300

    Register models on admin backend

--- apps/hub/admin.py
+++ apps/hub/admin.py
@@ -1,3 +1,36 @@
+"""Backend admin page."""
+
 from django.contrib import admin
 
-# Register your models here.
+from apps.hub.models import Category, Icon, Marker
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category model views on backend."""
+
+    list_display = ("id", "name", "icon", "color")
+    list_display_links = ("id", "name")
+    ordering = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Icon)
+class IconAdmin(admin.ModelAdmin):
+    """Icon model views on backend."""
+
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    ordering = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Marker)
+class MarkerAdmin(admin.ModelAdmin):
+    """Marker model views on backend."""
+
+    list_display = ("id", "latitude", "longitude", "category", "created_at")
+    list_display_links = ("id", "category")
+    list_filter = ("category",)
+    ordering = ("created_at", "category")
+    search_fields = ("text",)
