6be3760a99cb6f6e659c3c50a23590b30c252e2f
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-04 12:35:45 +0300

    Add home page test

--- apps/hub/tests.py
+++ apps/hub/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+"""Tests for HUB app."""
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from apps.hub.models import (
+    Marker,
+)
+
+
+class ViewsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def setUp(self):
+        print("setUp")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_index(self):
+        """Test home page with CarBrand list."""
+        response = self.client.get(path=reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "hub/index.html")
