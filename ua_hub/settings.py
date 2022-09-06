0aaa6475ac96930aab838f8745212c0d775f0163
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-06 20:19:03 +0300

    Add media and whitenoise settings

--- ua_hub/settings.py
+++ ua_hub/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -160,6 +161,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ua_hub/urls.py
+++ ua_hub/urls.py
@@ -13,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("apps.accounts.urls")),
     path("", include("apps.hub.urls")),
-    path('__debug__/', include('debug_toolbar.urls')),
-    re_path(r'^rosetta/', include('rosetta.urls')),
+    re_path(r"^rosetta/", include("rosetta.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
