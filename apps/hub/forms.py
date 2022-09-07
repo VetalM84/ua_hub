6d6772c5a29d519b74b16f89391bff531ae3e540
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-07 12:31:27 +0300

    Reformat code. Remove fields labels, add placeholders

--- apps/hub/forms.py
+++ apps/hub/forms.py
@@ -1,23 +1,43 @@
 """Form management for HUB app."""
 
 from django import forms
+from django.utils.translation import gettext as _
 
-from .models import Marker
+from .models import Category, Marker
 
 
 class AddMarkerForm(forms.ModelForm):
     """Form for adding a new marker on a frontend."""
+
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label=_("Выберите категорию"),
+        widget=forms.Select(attrs={"class": "form-select"}),
+    )
 
     class Meta:
         model = Marker
         fields = ["latitude", "longitude", "comment", "category"]
         widgets = {
             "latitude": forms.TextInput(
-                attrs={"class": "form-control", "id": "latitude"}
+                attrs={
+                    "class": "form-control",
+                    "id": "latitude",
+                    "placeholder": _("Широта"),
+                }
             ),
             "longitude": forms.TextInput(
-                attrs={"class": "form-control", "id": "longitude"}
+                attrs={
+                    "class": "form-control",
+                    "id": "longitude",
+                    "placeholder": _("Долгота"),
+                }
             ),
-            "comment": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
-            "category": forms.Select(attrs={"class": "form-select"}),
+            "comment": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 3,
+                    "placeholder": _("Комментарий, не обязательно"),
+                }
+            ),
         }
