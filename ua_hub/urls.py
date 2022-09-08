b5e426248bcb9048b716e51bcb076c05d99db287
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-08 21:56:11 +0300

    Add staticfiles

--- ua_hub/urls.py
+++ ua_hub/urls.py
@@ -30,3 +30,4 @@ if settings.DEBUG:
         path("__debug__/", include("debug_toolbar.urls")),
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
