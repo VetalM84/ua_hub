03d3c672841c3d0569accb45a590e386b24e49e5
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-08 10:38:04 +0300

    Add markers view on a frontend

--- apps/hub/views.py
+++ apps/hub/views.py
@@ -1,35 +1,36 @@
 """Hub app views."""
 
 import folium
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.utils.translation import gettext as _
-from folium import Popup
 from folium.features import LatLngPopup
 from folium.plugins import Fullscreen, LocateControl
 from jinja2 import Template
 
 from .forms import AddMarkerForm
+from .models import Marker
 
 
 def home(request):
     """Home page with map."""
-    form = AddMarkerForm()
 
     start_location = (50.45, 30.52)  # Ukraine
     current_map = folium.Map(location=start_location, zoom_start=6)
 
     # Fullscreen map button
-    Fullscreen(position="topright", title=_("Полный экран"), title_cancel=_("Выход")).add_to(
-        current_map
-    )
+    Fullscreen(
+        position="topright", title=_("Полный экран"), title_cancel=_("Выход")
+    ).add_to(current_map)
     # A button to define user's location
     LocateControl(
         auto_start=False, position="topright", strings={"title": _("Где я")}
     ).add_to(current_map)
 
     # Rewrite the default popup text to use custom popup with only coordinates
-    popup = LatLngPopup()
-    popup._template = Template(
+    # and auto latitude, longitude fields filling upon mouse click on a map
+    lat_lng_popup = LatLngPopup()
+    lat_lng_popup._template = Template(
         """
             {% macro script(this, kwargs) %}
                 var {{this.get_name()}} = L.popup();
@@ -45,17 +46,52 @@ def home(request):
             {% endmacro %}
             """
     )
-    popup.add_to(current_map)
+    lat_lng_popup.add_to(current_map)
 
     # deliver_layer = folium.FeatureGroup(name="Вручают").add_to(current_map)
+
+    html = """
+        <h1> This is a big popup</h1><br>
+        With a few lines of code...
+        <p>
+        <code>
+            from numpy import *<br>
+            exp(-2*pi)
+        </code>
+        </p>
+        """
+    iframe = folium.IFrame(html=html, width=1900, height=300)
 
     # Layer control button
     folium.LayerControl().add_to(current_map)
 
-    maps = current_map._repr_html_()
+    markers = Marker.objects.all().select_related()
+    for marker in markers:
+        folium.Marker(
+            location=(marker.latitude, marker.longitude),
+            popup=folium.Popup(iframe, max_width=400),
+            icon=folium.Icon(
+                color=marker.category.color.name, icon=marker.category.icon.name
+            ),
+            # tooltip=tooltip,
+        ).add_to(current_map)
 
-    context = {"maps": maps, "form": form}
+    if request.method == "POST":
+        form = AddMarkerForm(request.POST)
+        if form.is_valid():
+            try:
+                # form.save(commit=False)
+                # form.ip = get_client_ip(request)
+                form.save()
+                messages.success(request, _("Маркер успешно опубликован!"))
+                return redirect(to="home")
+            except Exception as e:
+                messages.error(request, _(f"Ошибка. {e}"))
+    else:
+        form = AddMarkerForm()
 
+    m = current_map._repr_html_()
+    context = {"current_map": m, "form": form}
     return render(request, template_name="index.html", context=context)
 
 
