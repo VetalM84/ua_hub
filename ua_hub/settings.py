b212164a360e11dfa47d6a87cf52b2a40e432ab0
Vitalii Mytenko <vetalmit@gmail.com>
2022-09-24 16:39:51 +0300

    Use redis cache

--- ua_hub/settings.py
+++ ua_hub/settings.py
@@ -183,9 +183,12 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CACHES = {
     "default": {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
-        # "BACKEND": "django.core.cache.backends.dummy.DummyCache",  # for developing
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "redis_hub"
     }
 }
 
