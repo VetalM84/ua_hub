ccc4291060ebd53e0f66a29d982e42c477204b62
Vitalii Mytenko <vetalmit@gmail.com>
2022-10-06 11:25:45 +0300

    Move DEFAULT_AUTO_FIELD upper

--- ua_hub/settings.py
+++ ua_hub/settings.py
@@ -196,6 +196,11 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # mail server settings
