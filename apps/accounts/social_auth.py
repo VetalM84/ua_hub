8d63b4e321f9336ec3c7a4a6d1b2ffb5a9140647
Vitalii Mytenko <vetalmit@gmail.com>
2022-10-13 22:25:29 +0300

    Save user image to model from social auth using facebook

--- apps/accounts/social_auth.py
+++ apps/accounts/social_auth.py
@@ -3,21 +3,23 @@
 import requests
 from django.core.files.base import ContentFile
 
-from apps.accounts.models import User
-
 
 def save_profile(backend, user, response, *args, **kwargs):
     """Save extra information to User instance upon singing in."""
+    name_from_email = user.email.split("@")
     if backend.name == "facebook":
-        profile = User.objects.get(pk=user.pk)
-        profile.facebook_link = response.get("link")
+        fb_img_url = f"https://graph.facebook.com/{response['id']}" \
+                     f"/picture?type=large&access_token={response['access_token']}"
+        image_content = ContentFile(requests.get(fb_img_url).content)
+        user.avatar.save(f"{name_from_email[0]}.jpg", image_content)
+        user.facebook_link = response.get("link")
         fb_hometown = response.get("hometown")
-        profile.hometown = fb_hometown["name"]
-        profile.save()
+        user.hometown = fb_hometown["name"]
+        user.save()
+
     elif backend.name == "google-oauth2":
-        profile = User.objects.get(pk=user.pk)
         # get image with higher resolution equal to 300 instead of 96
         google_img_url = response.get("picture").replace("s96-c", "s300-c")
-        name_from_email = user.email.split("@")
         image_content = ContentFile(requests.get(google_img_url).content)
-        profile.avatar.save(f"{name_from_email[0]}.jpg", image_content)
+        user.avatar.save(f"{name_from_email[0]}.jpg", image_content)
+        user.save()
