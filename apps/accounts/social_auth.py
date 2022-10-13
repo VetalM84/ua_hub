9781e1a487cfff6471c8ce6cd2911acae3367609
Vitalii Mytenko <vetalmit@gmail.com>
2022-10-13 21:45:31 +0300

    Save user image to model from social auth using google

--- apps/accounts/social_auth.py
+++ apps/accounts/social_auth.py
@@ -1,5 +1,8 @@
 """Functions to extend social authentication."""
 
+import requests
+from django.core.files.base import ContentFile
+
 from apps.accounts.models import User
 
 
@@ -11,3 +14,10 @@ def save_profile(backend, user, response, *args, **kwargs):
         fb_hometown = response.get("hometown")
         profile.hometown = fb_hometown["name"]
         profile.save()
+    elif backend.name == "google-oauth2":
+        profile = User.objects.get(pk=user.pk)
+        # get image with higher resolution equal to 300 instead of 96
+        google_img_url = response.get("picture").replace("s96-c", "s300-c")
+        name_from_email = user.email.split("@")
+        image_content = ContentFile(requests.get(google_img_url).content)
+        profile.avatar.save(f"{name_from_email[0]}.jpg", image_content)
