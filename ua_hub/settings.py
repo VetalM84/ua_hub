c4ad018ef9f9dff4ccbc86e0b22a9129af861837
Vitalii Mytenko <vetalmit@gmail.com>
2022-10-13 11:17:20 +0300

    Remove deprecated social-auth library call

--- ua_hub/settings.py
+++ ua_hub/settings.py
@@ -242,14 +242,14 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SOCIAL_AUTH_PIPELINE = (
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.user.get_username',
-    'social.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
     'apps.accounts.social_auth.save_profile',  # save extra info from Facebook response to user profile
     'social_core.pipeline.social_auth.associate_user',
-    'social.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
 
